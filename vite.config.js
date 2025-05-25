eff8004e9f23c55c794a89cfd79566c0fb89e430
kalinux0 <emir-baycan@hotmail.com>
2025-05-25 17:52:55 +0300

    publish

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
+
+export default defineConfig({
+    build: {
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/react/main.tsx',
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
